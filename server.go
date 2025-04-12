c26e1b08665dd995578dd5b7bd7d25e3a692799f
Motta <agoncale@emeal.nttdata.com>
2025-04-12 04:37:59 -0300

    feat: add Kubernetes deployment and health check to server
    This commit introduces Kubernetes configuration files for deploying the application, including ConfigMap, Pod, Service, Redis Deployment, ReplicaSet, and Deployment YAMLs. Additionally, the server.go file has been updated to include a health check endpoint and environment variable usage for dynamic content rendering. These changes enable the application to be deployed in a Kubernetes cluster with proper health monitoring and configuration management.

--- server.go
+++ server.go
@@ -1,13 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+var startedAt = time.Now()
 
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Projeto da Infnet! Feito por André Motta!"))
+	name := os.Getenv("NAME")
+	fmt.Fprint(w, "Oi, eu sou o "+name+"\n Projeto realizado para o curso da Infnet.")
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 20 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+
 }
