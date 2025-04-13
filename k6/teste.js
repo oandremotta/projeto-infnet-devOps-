371e097bf6b57c9d16f0de4f59140388bf149b23
Motta <agoncale@emeal.nttdata.com>
2025-04-13 02:56:09 -0300

    feat(k8s): update k6 load test configuration, add Grafana and Prometheus services, and implement Helm deployment workflow

--- k6/teste.js
+++ k6/teste.js
@@ -3,24 +3,40 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '30s', target: 50 }, // Aumenta a carga para 50 usuários ao longo de 30 segundos
-        { duration: '1m', target: 50 },  // Mantém 50 usuários por 1 minuto
-        { duration: '30s', target: 0 },  // Desce a carga para 0 usuários em 30 segundos
+        { duration: '1m', target: 50 },
+        { duration: '2m', target: 150 },
+        { duration: '2m', target: 200 },
+        { duration: '2m', target: 0 },
     ],
 };
 
 export default function () {
-    // A URL base da sua aplicação
-    const url = 'http://localhost:8000/healthz'; // Substitua com o endereço correto se for no Kubernetes
 
-    // Requisição HTTP
+    const url = 'http://localhost:8000/healthz';
+
+
     const res = http.get(url);
 
-    // Verificação do status da resposta (deve ser 200)
+
     check(res, {
         'status is 200': (r) => r.status === 200,
     });
 
-    // Sleep para simular usuários de forma mais realista
+
+    check(res, {
+        'response time is under 500ms': (r) => r.timings.duration < 500,
+    });
+
+
+    check(res, {
+        'response body contains "ok"': (r) => r.body.includes('ok'),
+    });
+
+
+    check(res, {
+        'content-type is application/json': (r) => r.headers['Content-Type'] === 'application/json',
+    });
+
+
     sleep(1);
 }
